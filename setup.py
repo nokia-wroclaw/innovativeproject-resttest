c3d80fd26d8a0f99dcc39ed5dfcbc44cbabf2016
Damian Mirecki <dmirecki@parisengineers.pl>
2014-11-07 22:25:04 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='resttest',
+    version='0.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Sławomir Domagała, Damian Mirecki, Tomasz Wlisłocki, Bartosz Zięba',
+    author_email='',
+    description='Tool for running rest-api tests written in plain language.'
+)
