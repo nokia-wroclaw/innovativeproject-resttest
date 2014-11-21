afc378cd6772d644f1dfeb74d8ac1a28bb9c8cc3
Sławomir Domagała <slawomir.domagala@nsn.com>
2014-11-21 12:56:49 +0100

    Extracted singleton

--- indor/src/result_collector.py
+++ indor/src/result_collector.py
@@ -1,12 +1,4 @@
-class Singleton(type):
-    def __init__(cls, name, bases, dic):
-        super(Singleton, cls).__init__(name, bases, dic)
-        cls.instance = None
-
-    def __call__(cls, *args, **kw):
-        if cls.instance is None:
-            cls.instance = super(Singleton, cls).__call__(*args, **kw)
-        return cls.instance
+from singleton import Singleton
 
 
 class ResultCollector(object):
