cf278ec4b85798513badc4369b4f60c7cf5229a2
slawomir <slawomir.karol.domagala@gmail.com>
2014-12-15 17:40:56 +0100

    Added arguments formatter

--- indor/src/main.py
+++ indor/src/main.py
@@ -1,21 +1,26 @@
 import sys
+from junit_xml_printer import JunitXMlPrinter
 from reading import read_from_file
 import test_runner
 import input_parser as parser
 from printer import Printer
-
-__author__ = 'slawomir'
-
+import argparse
 args = sys.argv
 
-if len(args) != 2:
-    print("Usage: python main.py file.ind")
-    sys.exit()
+arg_parser = argparse.ArgumentParser(description='Run REST tests')
+arg_parser.add_argument('file', type=str, nargs='?', help='File with definitions of tests')
+arg_parser.add_argument('--xml', dest='xml_output', action='store_true',
+                        help='Print the results in xml format')
 
-file_data = read_from_file(args[1])
+args = arg_parser.parse_args()
+
+file_data = read_from_file(args.file)
 
 test_data = parser.parse(file_data)
 runner = test_runner.TestsRunner()
 result = runner.run(test_data)
 
-Printer(result).print_summary()
+if args.xml_output:
+    JunitXMlPrinter(result).print_summary()
+else:
+    Printer(result).print_summary()
