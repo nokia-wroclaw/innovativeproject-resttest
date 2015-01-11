3133f4163772ceadfaa8cc2d22376bf9a5613374
Tomasz Wlislocki <twlislocki@parisengineers.pl>
2015-01-11 10:58:05 +0100

    Tests fix2

--- indor/src/scenario_data.py
+++ indor/src/scenario_data.py
@@ -1,5 +1,5 @@
 class ScenarioData(object):
-    def __init__(self, name, flags, repetition_name):
+    def __init__(self, name, flags, repetition_name=None):
         self.name = name
         self.flags = flags
         self.repetition_name = repetition_name
