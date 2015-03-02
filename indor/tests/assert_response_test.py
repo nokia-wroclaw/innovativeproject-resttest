3dcc1cc3dad0dbdea1a12b2b521b97b58670b663
slawomir <slawomir.karol.domagala@gmail.com>
2015-03-02 16:15:04 +0100

    Strange bug

--- indor/tests/assert_response_test.py
+++ indor/tests/assert_response_test.py
@@ -7,12 +7,12 @@ import mock
 
 class AssertResponseTest(unittest.TestCase):
 
-    @mock.patch('src.result_collector.ResultCollector')
+    @mock.patch('result_collector.ResultCollector')
     def test_path_empty_then_added_response_with_error(self, result_collector_mock):
         with self.assertRaises(Exception):
             CommandResponse(result_collector_mock).parse([])
 
-    @mock.patch('src.result_collector.ResultCollector')
+    @mock.patch('result_collector.ResultCollector')
     def test_path_incorrect_then_added_response_with_error(self, result_collector_mock):
         with self.assertRaises(Exception):
             CommandResponse(result_collector_mock).parse(["JasIMalgosia"])
