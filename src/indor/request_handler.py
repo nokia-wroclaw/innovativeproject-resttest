246bead155897093dd1c629a82ca98b53867f4bf
Sławomir Domagała <slawomir.domagala@nokia.com>
2017-02-23 16:40:39 +0100

    DynamicHTTPHandler handles every method

--- src/indor/request_handler.py
+++ src/indor/request_handler.py
@@ -1,5 +1,6 @@
 import http.server
 import urllib.parse
+from functools import partial
 from threading import Thread, Timer
 from time import sleep
 
@@ -18,7 +19,7 @@ class RequestHandler(object):
 
     def start(self):
         for key in self.handler_class.waiting_responses:
-            self.handler_class.waiting_responses[key].set_expiration_function(self.handler_class.pop_response_by_name)
+            self.handler_class.waiting_responses[key].set_expiration_function(self.handler_class.expire_response_by_name)
         for key in self.handler_class.waiting_responses:
             self.handler_class.waiting_responses[key].start_handling()
         self.server_thread.start()
@@ -43,6 +44,7 @@ class ExpirableResponse(object):
         self.url = url
         self.timer = None
         self.handled = False
+        self.request_method = None
         pass
 
     def get_response_code(self):
@@ -57,8 +59,9 @@ class ExpirableResponse(object):
     def match(self, path):
         return urllib.parse.urlparse(self.url).path == path
 
-    def set_handled(self):
+    def set_handled(self, request_method):
         self.handled = True
+        self.request_method = request_method
         self.timer.cancel()
 
     def set_expiration_function(self, func):
@@ -74,7 +77,7 @@ def _create_handler_class(resps):
         expired_responses = {}
 
         @classmethod
-        def pop_response_by_name(cls, name):
+        def expire_response_by_name(cls, name):
             cls.expired_responses[name] = cls.waiting_responses.pop(name)
             return cls.expired_responses[name]
 
@@ -82,18 +85,28 @@ def _create_handler_class(resps):
         def pop_response_by_url(cls, url):
             for key in cls.waiting_responses:
                 if cls.waiting_responses[key].match(url):
-                    cls.expired_responses[key] = cls.waiting_responses.pop(key)
-                    return cls.expired_responses[key]
+                    return cls.waiting_responses.pop(key)
             return None
 
-        def do_GET(self):
+        @classmethod
+        def put_expired_response(cls, resp):
+            cls.expired_responses[resp.name] = resp
+
+        def __getattr__(self, item):
+            if "do_" in item:
+                return partial(self.__do, item[2:])
+            raise AttributeError
+
+        def __do(self, method_name):
             resp = self.pop_response_by_url(self.path)
 
             if resp is None:
                 self.send_error(404, "URL <<" + self.path + ">> is not handled")
                 return
 
-            resp.set_handled()
+            resp.set_handled(method_name)
+            self.put_expired_response(resp)
+
             self.send_response(int(resp.get_response_code()))
             for key, value in resp.get_headers().items():
                 self.send_header(key, value)
